3cfbfa689a8159157d1434de4bfd3a6d848839ef
ksa237 <ksa237@yandex.ru>
2026-02-20 21:12:03 +1000

    init commit

---
+++ src/main/java/ru/netology/demoHiber/DemoHiberApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.demoHiber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHiberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHiberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/netology/demoHiber/DemoHiberApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.demoHiber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHiberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
