c2895338e716466cf08d11b1ffd5e5e9f9527e80
ksa237 <ksa237@yandex.ru>
2026-02-27 05:10:16 +1000

    hw4 jpa

--- src/main/java/ru/netology/demoHiber/controller/PersonController.java
+++ src/main/java/ru/netology/demoHiber/controller/PersonController.java
@@ -1,12 +1,13 @@
 package ru.netology.demoHiber.controller;
 
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.netology.demoHiber.service.PersonService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/persons")
@@ -18,16 +19,24 @@ public class PersonController {
         this.service = service;
     }
 
-
     @GetMapping()
-    public ResponseEntity<String> showConnectStatus(){
+    public ResponseEntity<String> showConnectStatus() {
         return ResponseEntity.ok("Server is working ok!");
     }
 
-
     @GetMapping("/by-city")
-    ResponseEntity<String> showPersonsByCity(@RequestParam("city") String city) {
-        return ResponseEntity.ok(service.findPersonsByCity(city).toString());
+    ResponseEntity<List<String>> showPersonsByCity(@RequestParam("city") String city) {
+        return ResponseEntity.ok(service.findPersonsByCity(city));
+    }
+
+    @GetMapping("/by-age")
+    ResponseEntity<List<String>> showPersonsAgeLess(@RequestParam("ageless") Integer age) {
+        return ResponseEntity.ok(service.findPersonsByAgeLess(age));
+    }
+
+    @GetMapping("/by-full-name")
+    ResponseEntity<String> showPersonByFullName(@RequestParam("name") String name, @RequestParam String surname) {
+        return ResponseEntity.ok(service.findPersonByFullName(name, surname));
     }
 
 }
--- src/main/java/ru/netology/demoHiber/service/PersonService.java
+++ src/main/java/ru/netology/demoHiber/service/PersonService.java
@@ -6,18 +6,52 @@ import ru.netology.demoHiber.domain.Person;
 import ru.netology.demoHiber.repository.PersonRepository;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
     private final PersonRepository repository;
 
-
     public PersonService(PersonRepository repository) {
         this.repository = repository;
     }
 
-    public List<Person> findPersonsByCity(String city) {
-        return repository.getPersonsByCity(city);
+    public List<String> findPersonsByCity(String city) {
+        List<Person> persons = repository.findByCityIgnoreCase(city);
+        return persons.stream()
+                .map(person -> String.format("%s %s, %d, %s, %s",
+                        person.getName(),
+                        person.getSurname(),
+                        person.getAge(),
+                        person.getPhone_number(),
+                        person.getCity())).collect(Collectors.toList());
+    }
+
+    public List<String> findPersonsByAgeLess(Integer age) {
+        List<Person> persons = repository.findByAgeLessThanOrderByAgeAsc(age);
+        return persons.stream()
+                .map(person -> String.format("%s %s, %d, %s, %s",
+                        person.getName(),
+                        person.getSurname(),
+                        person.getAge(),
+                        person.getPhone_number(),
+                        person.getCity())).collect(Collectors.toList());
+
+    }
+
+    public String findPersonByFullName(String name, String surname) {
+        Optional<Person> personOpt = repository.findFirst1ByNameAndSurnameAllIgnoreCase(name, surname);
+        if (personOpt.isEmpty()) {
+            return "Person not found";
+        }
+        Person person = personOpt.get();
+        return String.format("%s %s, %d, %s, %s",
+                person.getName(),
+                person.getSurname(),
+                person.getAge(),
+                person.getPhone_number(),
+                person.getCity());
     }
 
 }
